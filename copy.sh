d31dfd5d66c085bd49f951957655ac5894a3fcd0
Carlos Solís <csolisr@azkware.net>
2025-03-26 13:50:25 -0600

    fix: Remove intensive tasks by default from scheduled maintenance

--- copy.sh
+++ copy.sh
@@ -6,8 +6,10 @@ for i in ./*.sh; do
 	i_tmp="${i##./}"
 	find /etc/cron.* -iname "${i_tmp%.sh}" | while read -r j; do
 		echo "${j}"
-		diff <(sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/1\:-\"0\"/1\:-\"1\"/g" -e "s/#&>/\&\>/g" "${i}") "${j}"
-		sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/1\:-\"0\"/1\:-\"1\"/g" -e "s/#&>/\&\>/g" "${i}" | sudo tee "${j}" &>/dev/null
+		#diff <(sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/1\:-\"0\"/1\:-\"1\"/g" -e "s/#&>/\&\>/g" "${i}") "${j}"
+		#sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/1\:-\"0\"/1\:-\"1\"/g" -e "s/#&>/\&\>/g" "${i}" | sudo tee "${j}" &>/dev/null
+		diff <(sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/#&>/\&\>/g" "${i}") "${j}"
+		sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/#&>/\&\>/g" "${i}" | sudo tee "${j}" &>/dev/null
 	done
 	find ../../Scripts -iname "${i_tmp}" | while read -r k; do
 		echo "${k}"
