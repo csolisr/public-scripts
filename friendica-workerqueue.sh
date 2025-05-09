36014518cc5afa2d9ae3c2f15601e3ba1525452a
Carlos Solís <csolisr@azkware.net>
2025-05-09 01:56:24 +0000

    feat: Display Redis data about workerqueue

--- friendica-workerqueue.sh
+++ friendica-workerqueue.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo mariadb friendica --execute="select distinct command, count(*) from workerqueue where done = 0 group by command; select count(*) from workerqueue where done = 0; select count(*) from workerqueue where command = \"ProcessQueue\" and pid = 0 and done = 0"
+sudo redis-cli -n 1 keys '*' | cut -d":" -f2 | cut -d "-" -f 1-3 | sort | uniq -c | sort -n
