5161d010a816e1c459e576a1e1a7f91ecc3316fc
Carlos Solís <csolisr@azkware.net>
2025-08-24 13:53:40 -0600

    chore: Skip cron folders on my non-server devices

--- copy.sh
+++ copy.sh
@@ -7,13 +7,15 @@ for i in ./*.sh; do
 	shellcheck -o all -e SC2312 -f diff "${i}" | patch -p1
 	shellcheck -o all -e SC2312 "${i}"
 	i_tmp="${i##./}"
-	find /etc/cron.* -iname "${i_tmp%.sh}" | while read -r j; do
-		echo "${j}"
-		#diff <(sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/1\:-\"0\"/1\:-\"1\"/g" -e "s/#&>/\&\>/g" "${i}") "${j}"
-		#sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/1\:-\"0\"/1\:-\"1\"/g" -e "s/#&>/\&\>/g" "${i}" | sudo tee "${j}" &>/dev/null
-		diff <(sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/#&>/\&\>/g" "${i}") "${j}"
-		sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/#&>/\&\>/g" "${i}" | sudo tee "${j}" &>/dev/null
-	done
+	if [[ $(uname -n) == "azkware" || $(uname -n) == "azkware.net" ]]; then
+		find /etc/cron.* -iname "${i_tmp%.sh}" | while read -r j; do
+			echo "${j}"
+			#diff <(sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/1\:-\"0\"/1\:-\"1\"/g" -e "s/#&>/\&\>/g" "${i}") "${j}"
+			#sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/1\:-\"0\"/1\:-\"1\"/g" -e "s/#&>/\&\>/g" "${i}" | sudo tee "${j}" &>/dev/null
+			diff <(sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/#&>/\&\>/g" "${i}") "${j}"
+			sed -e "s/friendica.example.net/hub.azkware.net/g" -e "s/#&>/\&\>/g" "${i}" | sudo tee "${j}" &>/dev/null
+		done
+	fi
 	find ../../Scripts -iname "${i_tmp}" | while read -r k; do
 		echo "${k}"
 		diff <(sed -e "s/friendica.example.net/hub.azkware.net/g" "${i}") "${k}"
