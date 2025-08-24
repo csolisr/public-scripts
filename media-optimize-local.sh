5aba6ef3cdd94eb55ab981a6f8071b7c259eb547
Carlos Solís <csolisr@azkware.net>
2025-08-24 13:52:13 -0600

    feat: Refactor the loops for later reuse

--- media-optimize-local.sh
+++ media-optimize-local.sh
@@ -2,40 +2,66 @@
 IFS="
 "
 #media-optimize
+
+#jpeg
+p_jpeg_loop() {
+	quality=$(identify -verbose "${p}" | grep "Quality" | sed -e "s/.*Quality: //g")
+	if [[ ! -s "${quality}" ]]; then
+		quality="76"
+	fi
+	until grep -q -e "[OK]" -e ", skipped." <<<"${j}" || [[ -z "${j}" ]]; do
+		j=$(nice -n 15 jpegoptim -m "${quality}" "${p}")
+		echo "${j}" #&>/dev/null
+	done
+}
 while read -r p; do
-	nice -n 15 jpegoptim -m 76 "${p}" #&>/dev/null &
+	p_jpeg_loop "${p}" & #&>/dev/null &
 	if [[ $(jobs -r -p | wc -l) -ge $(getconf _NPROCESSORS_ONLN) ]]; then
 		wait -n
 	fi
-done < <(find . -type f -iname "*.jp*" \( -size +50k -and -mtime -8 \))
+done < <(find . -type f -iname "*.jp*" \( -size +10k \))
 wait
+
+#gif
+q_gif_loop() {
+	nice -n 15 gifsicle --batch -O3 --lossy=80 --colors=255 "${q}" #&>/dev/null
+}
 while read -r q; do
-	nice -n 15 gifsicle --batch -O3 --lossy=80 --colors=255 "${q}" #&>/dev/null &
+	q_gif_loop "${q}" & #&>/dev/null &
 	if [[ $(jobs -r -p | wc -l) -ge $(getconf _NPROCESSORS_ONLN) ]]; then
 		wait -n
 	fi
-done < <(find . -type f -iname "*.gif" \( -size +500k -and -mtime -8 \))
+done < <(find . -type f -iname "*.gif" \( -size +100k \))
 wait
-while read -r p; do
-	(while [[ $(stat -c%s "${p}" || 0) -ge 512000 ]]; do
+
+q_gif_recompress_loop() {
+	while [[ $(stat -c%s "${p}" || 0) -ge 512000 ]]; do
 		frameamount=$(($(exiftool -b -FrameCount "${p}" || 1) - 1))
 		nice -n 15 gifsicle "${p}" $(seq -f "#%g" 0 2 "${frameamount}") -O3 --lossy=80 --colors=255 -o "${p}" #&>/dev/null
-	done) &
+	done
+}
+while read -r p; do
+	q_gif_recompress_loop "${p}" &
 	if [[ $(jobs -r -p | wc -l) -ge $(getconf _NPROCESSORS_ONLN) ]]; then
 		wait -n
 	fi
 done < <(find . -type f -size +500k \( -iname "*-320.gif" -or -iname "*-80.gif" -or -iname "*-48.gif" \))
 wait
+
+#png
+r_png_loop() {
+	nice -n 15 oxipng -o max "${r}" #&>/dev/null
+}
 while read -r r; do
-	nice -n 15 oxipng -o max "${r}" #&>/dev/null &
+	r_png_loop "${r}" & #&>/dev/null &
 	if [[ $(jobs -r -p | wc -l) -ge $(getconf _NPROCESSORS_ONLN) ]]; then
 		wait -n
 	fi
-done < <(find . -type f -iname "*.png" -size +500k)
-
+done < <(find . -type f -iname "*.png" -size +100k)
 wait
-#compress-webp
-while read -r s; do
+
+#webp
+s_webp_loop() {
 	#If file is not animated
 	if [[ -f "${s}" ]]; then
 		if grep -v -q -e "ANIM" -e "ANMF" "${s}"; then
@@ -51,89 +77,71 @@ while read -r s; do
 			fi
 		fi
 	fi
-done < <(find . -type f -iname "*.webp" \( -size +50k -and -mtime -8 \))
+}
+while read -r s; do
+	s_webp_loop "${s}" & #&>/dev/null &
+	if [[ $(jobs -r -p | wc -l) -ge $(getconf _NPROCESSORS_ONLN) ]]; then
+		wait -n
+	fi
+done < <(find . -type f -iname "*.webp" \( -size +10k \))
 wait
+
+#Fix extensions
 while read -r i; do
 	if file -b "${i}" | grep -vq "PNG" | grep -vq "empty" | grep -vq "symbolic link" | grep -vq "directory" | grep -vq "text"; then
 		if file -b "${i}" | grep -q "JPEG"; then
 			mv "${i}" "${i%.*}".jpg
-			jpegoptim -m76 "${i%.*}".jpg #&>/dev/null
+			#p="${i%.*}.jpg"
+			#p_jpeg_loop "${p}"
+			#jpegoptim -m76 "${i%.*}".jpg #&>/dev/null
 		elif file "${i}" | grep -q "GIF"; then
 			mv "${i}" "${i%.*}".gif
-			gifsicle --batch -O3 --lossy=80 --colors=255 "${i%.*}".gif #&>/dev/null
+			#gifsicle --batch -O3 --lossy=80 --colors=255 "${i%.*}".gif #&>/dev/null
 		elif file "${i}" | grep -q "Web/P"; then
 			mv "${i}" "${i%.*}".webp
 			#cwebp -mt -af -quiet $i
 		fi
 	fi
-done < <(find . -iname "*.png" -mtime -8)
+done < <(find . -iname "*.png")
 while read -r j; do
 	if file -b "${j}" | grep -v -q -e "JPEG" -e "empty" -e "symbolic link" -e "directory" -e "text"; then
 		if file -b "${j}" | grep -q "PNG"; then
 			mv "${j}" "${j%.*}".png
-			oxipng -o max "${j%.*}".png #&>/dev/null
+			#oxipng -o max "${j%.*}".png #&>/dev/null
 		elif file -b "${j}" | grep -q "GIF"; then
 			mv "${j}" "${j%.*}".gif
-			gifsicle --batch -O3 --lossy=80 --colors=255 "${j%.*}".gif #&>/dev/null
+			#gifsicle --batch -O3 --lossy=80 --colors=255 "${j%.*}".gif #&>/dev/null
 		elif file -b "${j}" | grep -q "Web/P"; then
 			mv "${j}" "${j%.*}".webp
-			#If file is not animated
-			if [[ -f "${j}" ]]; then
-				if grep -v -q -e "ANIM" -e "ANMF" "${s}"; then
-					cwebp -mt -af -quiet "${s}" -o /tmp/"${s##.*\/}"_temp.webp #&>/dev/null
-					if [[ -f /tmp/"${s##.*\/}"_temp.webp ]]; then
-						size_new=$(stat -c%s /tmp/"${s##.*\/}"_temp.webp || 0)
-						size_original=$(stat -c%s "${s}" || 0)
-						if [[ -n "${size_original}" && -n "${size_new}" && "${size_original}" -gt "${size_new}" ]]; then
-							mv /tmp/"${s##.*\/}"_temp.webp "${s}"
-						else
-							rm /tmp/"${s##.*\/}"_temp.webp
-						fi
-					fi
-				fi
-			fi
+			#cwebp -mt -af -quiet $i
 		fi
 	fi
-done < <(find . -mtime -8 \( -iname "*.jpg" -and -iname "*.jpeg" \))
+done < <(find . \( -iname "*.jpg" -or -iname "*.jpeg" \))
 while read -r k; do
 	if file -b "${k}" | grep -v -q -e "GIF" -e "empty" -e "symbolic link" -e "directory" -e "text"; then
 		if file -b "${k}" | grep -q "JPEG"; then
 			mv "${k}" "${k%.*}".jpg
-			jpegoptim -m76 "${k%.*}".jpg #&>/dev/null
+			#jpegoptim -m76 "${k%.*}".jpg #&>/dev/null
 		elif file -b "${k}" | grep -q "PNG"; then
 			mv "${k}" "${k%.*}".png
-			oxipng -o max "${k%.*}".png #&>/dev/null
+			#oxipng -o max "${k%.*}".png #&>/dev/null
 		elif file -b "${k}" | grep -q "Web/P"; then
 			mv "${k}" "${k%.*}".webp
-			#If file is not animated
-			if [[ -f "${k}" ]]; then
-				if grep -v -q -e "ANIM" -e "ANMF" "${s}"; then
-					cwebp -mt -af -quiet "${s}" -o /tmp/"${s##.*\/}"_temp.webp #&>/dev/null
-					if [[ -f /tmp/"${s##.*\/}"_temp.webp ]]; then
-						size_new=$(stat -c%s /tmp/"${s##.*\/}"_temp.webp || 0)
-						size_original=$(stat -c%s "${s}" || 0)
-						if [[ -n "${size_original}" && -n "${size_new}" && "${size_original}" -gt "${size_new}" ]]; then
-							mv /tmp/"${s##.*\/}"_temp.webp "${s}"
-						else
-							rm /tmp/"${s##.*\/}"_temp.webp
-						fi
-					fi
-				fi
-			fi
+			#cwebp -mt -af -quiet $i
 		fi
 	fi
-done < <(find . -iname "*.gif" -mtime -8)
+done < <(find . -iname "*.gif")
 while read -r l; do
 	if file -b "${l}" | grep -v -q -e "Web/P" -e "empty" -e "symbolic link" -e "directory" -e "text"; then
 		if file -b "${l}" | grep -q "JPEG"; then
 			mv "${l}" "${l%.*}".jpg
-			jpegoptim -m76 "${l%.*}".jpg #&>/dev/null
+			#jpegoptim -m76 "${l%.*}".jpg #&>/dev/null
 		elif file -b "${l}" | grep -q "PNG"; then
 			mv "${l}" "${l%.*}".png
-			oxipng -o max "${l%.*}".png #&>/dev/null
+			#oxipng -o max "${l%.*}".png #&>/dev/null
 		elif file -b "${l}" | grep -q "GIF"; then
 			mv "${l}" "${l%.*}".gif
-			gifsicle --batch -O3 --lossy=80 --colors=255 "${l%.*}".gif #&>/dev/null
+			#gifsicle --batch -O3 --lossy=80 --colors=255 "${l%.*}".gif #&>/dev/null
 		fi
 	fi
-done < <(find . -iname "*.webp" -mtime -8)
+done < <(find . -iname "*.webp")
