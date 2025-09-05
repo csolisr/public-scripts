5e30235a1452c0dbec4795c93b3e23e59de8b0b0
Carlos Solís <csolisr@azkware.net>
2025-09-05 14:41:59 +0000

    feat: Show information by date; parameterize database; test for DB engine

--- friendica-workerqueue.sh
+++ friendica-workerqueue.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-sudo mariadb friendica --execute="select distinct command, count(*) from workerqueue where done = 0 group by command; select count(*) from workerqueue where done = 0; select count(*) from workerqueue where command = \"ProcessQueue\" and pid = 0 and done = 0"
-sudo redis-cli -n 1 keys '*' | cut -d":" -f2 | cut -d "-" -f 1-3 | sort | uniq -c | sort -n
+dbname="friendica"
+dbengine="mysql"
+if [[ -n $(type mariadb) ]]; then
+	dbengine="mariadb"
+elif [[ -n $(type mysql) ]]; then
+	dbengine="mysql"
+else
+	echo "MySQL not found." && exit
+fi
+sudo "${dbengine}" "${dbname}" --execute="select distinct command, date(created), count(*) from workerqueue where done = 0 group by command, date(created); select distinct command, count(*) from workerqueue where done = 0 group by command having count(*) > 1; select count(*) as \"Full Count\" from workerqueue where done = 0; select count(*) as \"ProcessQueue Count\" from workerqueue where command = \"ProcessQueue\" and pid = 0 and done = 0"
+if [[ -n $(type redis-cli) ]]; then
+	sudo redis-cli -n 1 keys '*' | cut -d":" -f2 | cut -d "-" -f 1-3 | sort | uniq -c | sort -n | sed -e "/ 1 .*/d"
+fi
