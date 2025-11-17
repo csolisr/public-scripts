d379094373ab08cdc123c839174196d777006056
Carlos Solís <csolisr@azkware.net>
2025-11-17 16:16:16 +0000

    feat: Add support for multiple language trending posts, Misskey API, manual overrides

--- trending-hashtags.sh
+++ trending-hashtags.sh
@@ -5,12 +5,14 @@ if [[ $(uname -n) == "azkware" ]]; then
 else
 	localmode=${1:-"1"}
 fi
-#URL of your instance. Can pass as the second parameter.
-mysite=${2:-"friendica.example.com"}
-#Token from your website. Can pass as the third parameter. Needs to be generated using something like GetAuth and the "read" permission ( https://getauth.thms.uk/?scopes=read )
-token=${3:-"12345678"}
-#URL of the service that contains the list of servers to fetch. Can pass as the fourth parameter.
-serverslist=${4:-"https://api.fedidb.org/v1/servers?limit=40"}
+#Limit of servers to fetch. Can pass as the second parameter.
+limit=${2:-"20"}
+#URL of your instance. Can pass as the third parameter.
+mysite=${3:-"friendica.example.net"}
+#Token from your website. Can pass as the fourth parameter. Needs to be generated using something like GetAuth and the "read" permission ( https://getauth.thms.uk/?scopes=read )
+token=${4:-"12345678"}
+#URL of the service that contains the list of servers to fetch. Can pass as the fifth parameter.
+serverslist=${5:-"https://api.fedidb.org/v1/servers?limit=${limit}"}
 #Tweak this if your instance uses a non-standard API.
 searchurl="https://${mysite}/api/v2/search?resolve=true&limit=1&type=statuses&q="
 #File that will hold the URLs found.
@@ -25,6 +27,10 @@ tags_file="/tmp/trending_hashtags.txt"
 threads=$(($(getconf _NPROCESSORS_ONLN) * 2))
 #User agent (to be used to identify the process)
 useragent="Trending Hashtags Fetcher (https://${mysite})"
+#Languages for the trending topics, in ISO format.
+languages=("en-US" "es-ES" "ja-JP" "de-DE" "fr-FR")
+#Manual overrides for some external services, such as bridges, generally blocked by some servers.
+overrides=("threads.net" "threads.com" "threads.instagram.com" "bsky.brid.gy" "bird.makeup" "mostr.pub" "newsmast.org" "newsmast.social" "mastodon.social" "misskey.io" "misskey.gg" "mstdn.jp" "mastodon.cloud" "mastodon.world" "fosstodon.org" "mas.to" "mastodon.art" "troet.cafe" "mastodon.online")
 
 fetch_sites() {
 	while read -r searchsite; do
@@ -50,14 +56,14 @@ fetch_blocks() {
 		fi
 		sort "${block_file}" | uniq -i >"${block_file}.tmp" && mv "${block_file}.tmp" "${block_file}"
 	fi
-	#Remove found URLs from the block list, since some smaller instances block the larger instances solely due to their size.
+	#Override URLs from the block list, since some smaller instances block bridging services, corporate servers, or even larger instances solely due to their size.
 	if [[ -f "${block_file}" && -f "${found_file}" ]]; then
 		if [[ "${localmode}" != "0" ]]; then
 			echo "Reinstating domains..." #&> /dev/null
 		fi
-		while read -r url_to_ignore; do
-			grep -v -F -e "${url_to_ignore}" -- "${block_file}" >"${block_file}.tmp" && mv "${block_file}.tmp" "${block_file}"
-		done <"${found_file}"
+		for override in "${overrides[@]}"; do
+			grep -v -F -e "${override}" -- "${block_file}" >"${block_file}.tmp" && mv "${block_file}.tmp" "${block_file}"
+		done
 	fi
 	#Print amount of blocked URLs
 	count_file="${block_file}"
@@ -67,7 +73,7 @@ fetch_blocks() {
 
 fetch_block() {
 	while read -r block_to_add; do
-		if [[ "${block_to_add}" != "null" && -f "${block_file}" ]]; then
+		if [[ "${block_to_add}" != "null" && -f "${block_file}" && -z $(echo "${block_to_add}" | awk "/\*/") ]]; then
 			if [[ "${localmode}" != "0" ]]; then
 				echo "Blocked URL:" "${block_to_add}" #&> /dev/null
 			fi
@@ -78,18 +84,63 @@ fetch_block() {
 
 fetch_hashtags() {
 	while read -r searchsite; do
+		did_add_hashtag=0
 		while read -r hashtag_to_add; do
-			already_printed=0
-			if [[ -f "${tags_file}" ]]; then
-				if [[ "${already_printed}" -eq 0 ]]; then
-					if [[ "${localmode}" != "0" ]]; then
-						echo "Site: ${searchsite} Hashtag: #${hashtag_to_add}" #&> /dev/null
+			if [[ -n "${hashtag_to_add}" && "${hashtag_to_add}" != "null" ]]; then
+				already_printed=0
+				if [[ -f "${tags_file}" ]]; then
+					if [[ "${already_printed}" -eq 0 ]]; then
+						if [[ "${localmode}" != "0" ]]; then
+							echo "Site: ${searchsite} Hashtag: #${hashtag_to_add}" #&> /dev/null
+						fi
+						already_printed=1
 					fi
-					already_printed=1
+					echo "${hashtag_to_add}" >>"${tags_file}"
+					if [[ -n "${hashtag_to_add}" ]]; then
+						did_add_hashtag=1
+					fi
 				fi
-				echo "${hashtag_to_add}" >>"${tags_file}"
 			fi
 		done < <(curl -s -S --no-progress-meter -L -H "User-Agent: ${useragent}" "https://${searchsite}/api/v1/trends/tags" 2>/dev/null | jq -r '.[].name' 2>/dev/null)
+		#If this returns nothing, fall back to the Misskey API
+		if [[ "${did_add_hashtag}" -eq 0 ]]; then
+			while read -r hashtag_to_add; do
+				if [[ -n "${hashtag_to_add}" && "${hashtag_to_add}" != "null" ]]; then
+					already_printed=0
+					if [[ -f "${tags_file}" ]]; then
+						if [[ "${already_printed}" -eq 0 ]]; then
+							if [[ "${localmode}" != "0" ]]; then
+								echo "Site: ${searchsite} (Misskey API) Hashtag: #${hashtag_to_add}" #&> /dev/null
+							fi
+							already_printed=1
+						fi
+						echo "${hashtag_to_add}" >>"${tags_file}"
+					fi
+				fi
+			done < <(curl -s -S --no-progress-meter -L -H "User-Agent: ${useragent}" "https://${searchsite}/api/hashtags/trend" 2>/dev/null | jq -r '.[].tag' 2>/dev/null)
+		fi
+		#Other APIs I should check:
+		#For Misskey:
+		#	There doesn't seem to be a specific blocked domains API, besides of the Mastodon compatibility layer.
+		#For Pixelfed:
+		#	There are backends for everything, but they seem to require an account on each server for authentication.
+		#	For trending hashtags:
+		#		"https://${searchsite}/api/v1.1/discover/posts/hashtags"
+		#	For trending posts:
+		#		"https://${searchsite}/api/v1.1/discover/posts/trending"
+		#	For blocked domains:
+		#		"https://${searchsite}/api/v1/domain_blocks"
+		#For Lemmy:
+		#	There is an API to post block instances as admin, but not to read them.
+		#	Also, there are no hashtags, but there are trending posts that can be accessed via RSS.
+		#For PeerTube:
+		#	For blocked domains:
+		#		"https://${searchsite}/api/v1/server/blocklist/servers"
+		#	There are no hashtags, but there are trending posts.
+		#	The search API requires some text to search for, however, so this might not work to fetch trending videos per se.
+		#		"https://${searchsite}/api/v1/search/videos?sort=-trending&count=100&isLocal=false&search=*"
+		#			From here you will need to fetch pairs of '.data[].uuid' and '.data[].channel.host' to get the actual URL, in the form of
+		#			"https://${host}/videos/watch/${uuid}"
 	done < <(echo "${serversresponse}" | jq -r '.data[].domain')
 	wait
 	#Deduplicate
@@ -118,38 +169,60 @@ fetch_hashtags() {
 
 fetch_hashtag() {
 	if [[ "${hashtag_to_add}" != "null" ]]; then
+		did_add_url=0
 		already_printed=0
 		#Fetch the URLs that contain the hashtag, and populate your website with them
 		while read -r url_to_fetch; do
-			if [[ "${already_printed}" -eq 0 ]]; then
-				if [[ "${localmode}" != "0" ]]; then
-					echo "Site: ${searchsite} Hashtag: #${hashtag_to_add}" #&> /dev/null
+			if [[ -n "${url_to_fetch}" && "${url_to_fetch}" != "null" ]]; then
+				if [[ "${already_printed}" -eq 0 ]]; then
+					if [[ "${localmode}" != "0" ]]; then
+						echo "Site: ${searchsite} Hashtag: #${hashtag_to_add}" #&> /dev/null
+					fi
+					already_printed=1
 				fi
-				already_printed=1
+				fetch_url "${url_to_fetch}" "${searchsite}"
+				if [[ -n "${url_to_fetch}" ]]; then
+					did_add_url=1
+				fi
 			fi
-			fetch_url "${url_to_fetch}" "${searchsite}"
 		done < <(curl -s -S --no-progress-meter -L -H "User-Agent: ${useragent}" "https://${searchsite}/api/v1/timelines/tag/${hashtag_to_add}?local=false" 2>/dev/null | jq -r '.[].uri' 2>/dev/null)
+		#If no URLs are found, fall back to the Misskey API
+		if [[ "${did_add_url}" -eq 0 ]]; then
+			while read -r url_to_fetch; do
+				if [[ -n "${url_to_fetch}" && "${url_to_fetch}" != "null" ]]; then
+					if [[ "${already_printed}" -eq 0 ]]; then
+						if [[ "${localmode}" != "0" ]]; then
+							echo "Site: ${searchsite} (Misskey API) Hashtag: #${hashtag_to_add}" #&> /dev/null
+						fi
+						already_printed=1
+					fi
+					fetch_url "${url_to_fetch}" "${searchsite}"
+				fi
+			done < <(curl -s -S --no-progress-meter -L -H "User-Agent: ${useragent}" -H 'Content-Type: application/json' -d "{\"query\": \"#${hashtag_to_add}\"}" "https://${searchsite}/api/notes/search" 2>/dev/null | jq -r '.[].uri' 2>/dev/null)
+		fi
 	fi
 }
 
 fetch_trending_posts() {
 	while read -r searchsite; do
-		already_printed=0
-		while read -r url_to_fetch; do
-			if [[ "${url_to_fetch}" != "null" ]]; then
-				if [[ "${already_printed}" -eq 0 ]]; then
-					if [[ "${localmode}" != "0" ]]; then
-						echo "Site: ${searchsite} Trending posts" #&> /dev/null
+		for language in "${languages[@]}"; do
+			already_printed=0
+			while read -r url_to_fetch; do
+				if [[ "${url_to_fetch}" != "null" ]]; then
+					if [[ "${already_printed}" -eq 0 ]]; then
+						if [[ "${localmode}" != "0" ]]; then
+							echo "Site: ${searchsite} Trending posts (${language})" #&> /dev/null
+						fi
+						already_printed=1
 					fi
-					already_printed=1
-				fi
-				fetch_url "${url_to_fetch}" &
-				if [[ $(jobs -r -p | wc -l) -ge "${threads}" ]]; then
-					wait -n
-				fi
+					fetch_url "${url_to_fetch}" &
+					if [[ $(jobs -r -p | wc -l) -ge "${threads}" ]]; then
+						wait -n
+					fi
 
-			fi
-		done < <(curl -s -S --no-progress-meter -L -H "User-Agent: ${useragent}" "https://${searchsite}/api/v1/trends/statuses" 2>/dev/null | jq -r '.[].uri' 2>/dev/null)
+				fi
+			done < <(curl -s -S --no-progress-meter -L -H "User-Agent: ${useragent}" -H "Accept-Language: ${language}" "https://${searchsite}/api/v1/trends/statuses" 2>/dev/null | jq -r '.[].uri' 2>/dev/null)
+		done
 	done < <(echo "${serversresponse}" | jq -r '.data[].domain')
 	wait
 }
