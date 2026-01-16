ea5f70345559cf43bef4e6096e1c196ff6fbc371
Carlos Solís <csolisr@azkware.net>
2026-01-16 19:27:46 +0000

    feat: Optimize the deletion, change defaults

--- friendica-delete-old-posts.sh
+++ friendica-delete-old-posts.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 #Enables optimizations to make the deletion take less time. Set to 0 to delete more items. Set to 2 to also delete items from your followed accounts (this will break reposts and mentions)
 intense_optimizations=${1:-"1"}
-#Determines the interval of deletion. Defaults to 365 days.
-i=${2:-"365"}
+#Determines the interval of deletion. Defaults to 90 days, same as Friendica.
+i=${2:-"90"}
 interval="${i} DAY"
 dbengine="mariadb"
 db="friendica"
+
+#First, search all reserved matches where:
+#- the post is from a followed contact,
+#- the post if from a user of this instance, whether due to the "user" or "contact" IDs,
+#- or the post is from a member of a group.
+
+sudo "${dbengine}" "${db}" -vvve "\
+	drop table if exists tmp_post_matches; \
+	create table tmp_post_matches (\
+		select distinct p.\`uri-id\` from \`post\` p \
+		where p.\`author-id\` in (select \`cid\` from \`user-contact\`) \
+		or p.\`causer-id\` in (select \`cid\` from \`user-contact\`) \
+		or p.\`owner-id\` in (select \`cid\` from \`user-contact\`) \
+		or p.\`author-id\` in (select \`uid\` from \`user\` where \`uid\` != \"0\") \
+		or p.\`causer-id\` in (select \`uid\` from \`user\` where \`uid\` != \"0\") \
+		or p.\`owner-id\` in (select \`uid\` from \`user\` where \`uid\` != \"0\") \
+		or p.\`author-id\` in (select \`id\` from \`contact\` where \`gsid\` = \"1\") \
+		or p.\`causer-id\` in (select \`id\` from \`contact\` where \`gsid\` = \"1\") \
+		or p.\`owner-id\` in (select \`id\` from \`contact\` where \`gsid\` = \"1\") \
+		or p.\`author-id\` in (select \`contact-id\` from \`group_member\`) \
+		or p.\`causer-id\` in (select \`contact-id\` from \`group_member\`) \
+		or p.\`owner-id\` in (select \`contact-id\` from \`group_member\`) \
+	);" # &> /dev/null
 
 #Show only in case we're not optimizing for speed.
 if [[ "${intense_optimizations}" -eq 0 ]]; then
 	#For safety, find all the posts that would somehow match both criteria and thus be wrongly deleted. We want this to return no results.
 	#Then, show how many posts would be deleted this way, sorted by date.
 	sudo "${dbengine}" "${db}" -vvve "\
-		create temporary table tmp_post_matches (\
-			select distinct p.\`uri-id\` from \`post\` p \
-			where p.\`author-id\` in (select \`cid\` from \`user-contact\`) \
-			or p.\`causer-id\` in (select \`cid\` from \`user-contact\`) \
-			or p.\`owner-id\` in (select \`cid\` from \`user-contact\`) \
-			or p.\`author-id\` in (select \`uid\` from \`user\` where \`uid\` != \"0\") \
-			or p.\`causer-id\` in (select \`uid\` from \`user\` where \`uid\` != \"0\") \
-			or p.\`owner-id\` in (select \`uid\` from \`user\` where \`uid\` != \"0\") \
-			or p.\`author-id\` in (select \`id\` from \`contact\` where \`gsid\` = \"1\") \
-			or p.\`causer-id\` in (select \`id\` from \`contact\` where \`gsid\` = \"1\") \
-			or p.\`owner-id\` in (select \`id\` from \`contact\` where \`gsid\` = \"1\") \
-			or p.\`author-id\` in (select \`contact-id\` from \`group_member\`) \
-			or p.\`causer-id\` in (select \`contact-id\` from \`group_member\`) \
-			or p.\`owner-id\` in (select \`contact-id\` from \`group_member\`) \
-		); \
 		create temporary table tmp_post (\
 			select distinct p.\`uri-id\` from \`post\` p \
 			where p.\`uri-id\` not in (select \`uri-id\` from \`tmp_post_matches\`) \
@@ -46,32 +54,13 @@ if [[ "${intense_optimizations}" -eq 0 ]]; then
 		select count(p.\`uri-id\`), p.\`created\` from \`post\` p \
 			inner join tmp_post tmp on p.\`uri-id\` = tmp.\`uri-id\` \
 			group by date_format(p.\`created\`, \"%y%m\") \
-			order by \`created\` desc;"
+			order by \`created\` desc;" # &> /dev/null
 fi
 
 if [[ "${intense_optimizations}" -lt 2 ]]; then
-	#First, search all reserved matches where:
-	#- the post is from a followed contact,
-	#- the post if from a user of this instance, whether due to the "user" or "contact" IDs,
-	#- or the post is from a member of a group.
-	#Then, filter all the non-matching items (which we do want to delete) that are older than the given interval.
+	#Filter all the non-matching items (which we do want to delete) that are older than the given interval.
 	#Finally, proceed with the deletion.
 	sudo "${dbengine}" "${db}" -vvve "\
-		create temporary table tmp_post_matches (\
-			select distinct p.\`uri-id\` from \`post\` p \
-			where p.\`author-id\` in (select \`cid\` from \`user-contact\`) \
-			or p.\`causer-id\` in (select \`cid\` from \`user-contact\`) \
-			or p.\`owner-id\` in (select \`cid\` from \`user-contact\`) \
-			or p.\`author-id\` in (select \`uid\` from \`user\` where \`uid\` != \"0\") \
-			or p.\`causer-id\` in (select \`uid\` from \`user\` where \`uid\` != \"0\") \
-			or p.\`owner-id\` in (select \`uid\` from \`user\` where \`uid\` != \"0\") \
-			or p.\`author-id\` in (select \`id\` from \`contact\` where \`gsid\` = \"1\") \
-			or p.\`causer-id\` in (select \`id\` from \`contact\` where \`gsid\` = \"1\") \
-			or p.\`owner-id\` in (select \`id\` from \`contact\` where \`gsid\` = \"1\") \
-			or p.\`author-id\` in (select \`contact-id\` from \`group_member\`) \
-			or p.\`causer-id\` in (select \`contact-id\` from \`group_member\`) \
-			or p.\`owner-id\` in (select \`contact-id\` from \`group_member\`) \
-		); \
 		create temporary table tmp_post (\
 			select distinct p.\`uri-id\` from \`post\` p \
 			where p.\`uri-id\` not in (select \`uri-id\` from \`tmp_post_matches\`) \
@@ -93,11 +82,7 @@ fi
 #Show only in case we're not optimizing for speed.
 if [[ "${intense_optimizations}" -eq 0 ]]; then
 	#Find any posts created before the first user of the instance registered.
-	#For safety, search all reserved matches where:
-	#- the post is from a followed contact,
-	#- the post if from a user of this instance, whether due to the "user" or "contact" IDs,
-	#- or the post is from a member of a group.
-	#Find all the posts that would somehow match the criteria and thus be wrongly deleted. We want this to return no results.
+	#For safety, search all the reserved matches, then find all the posts that would somehow match the criteria and thus be wrongly deleted. We want this to return no results.
 	#Then, show how many posts would be deleted this way, sorted by date.
 	#Finally, proceed with the deletion.
 	sudo "${dbengine}" "${db}" -vvve "\
@@ -144,7 +129,7 @@ if [[ "${intense_optimizations}" -eq 2 ]]; then
 	#Then, show how many posts would be deleted this way, sorted by date.
 	#Finally, proceed with the deletion.
 	sudo "${dbengine}" "${db}" -vvve "\
-		create temporary table tmp_post_matches (\
+		create temporary table tmp_post_matches_unrestricted (\
 			select distinct p.\`uri-id\` from \`post\` p \
 			where p.\`author-id\` in (select \`uid\` from \`user\` where \`uid\` != \"0\") \
 			or p.\`causer-id\` in (select \`uid\` from \`user\` where \`uid\` != \"0\") \
@@ -155,7 +140,7 @@ if [[ "${intense_optimizations}" -eq 2 ]]; then
 		); \
 		create temporary table tmp_post (\
 			select distinct p.\`uri-id\` from \`post\` p \
-			where p.\`uri-id\` not in (select \`uri-id\` from \`tmp_post_matches\`) \
+			where p.\`uri-id\` not in (select \`uri-id\` from \`tmp_post_matches_unrestricted\`) \
 			and p.\`created\` < CURDATE() - INTERVAL ${interval} \
 		); \
 		select t.\`id\`, t.\`name\`, t.\`addr\`, p.\`created\`, c.\`body\` from \`tmp_post\` tmp \
@@ -185,10 +170,13 @@ if [[ "${intense_optimizations}" -eq 2 ]]; then
 		delete from \`post\` where \`uri-id\` in (select \`uri-id\` from \`tmp_post\`); \
 	" # &> /dev/null
 fi
-#Show all the leftover posts, sorted by date.
-sudo "${dbengine}" "${db}" -e "select count(*), \`created\` from \`post\` group by date_format(\`created\`, \"%y%m\") order by \`created\` desc" # &> /dev/null
 
-#Fix the auto-increment for the affected tables.
+if [[ "${intense_optimizations}" -ne 1 ]]; then
+	#Show all the leftover posts, sorted by date.
+	sudo "${dbengine}" "${db}" -e "select count(*), \`created\` from \`post\` group by date_format(\`created\`, \"%y%m\") order by \`created\` desc" # &> /dev/null
+fi
+
+#Fix the auto-increment for the affected tables. Clear temporary tables.
 sudo "${dbengine}" "${db}" -v -e "\
 	alter table \`post-thread\` auto_increment = 1; \
 	alter table \`post-thread-user\` auto_increment = 1; \
@@ -199,4 +187,5 @@ sudo "${dbengine}" "${db}" -v -e "\
 	alter table \`post-counts\` auto_increment = 1; \
 	alter table \`post-category\` auto_increment = 1; \
 	alter table \`post-history\` auto_increment = 1; \
-	alter table \`post\` auto_increment = 1;"
+	alter table \`post\` auto_increment = 1; \
+	drop table if exists tmp_post_matches;" # &> /dev/null
