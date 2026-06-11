92ddf567d0cff5e7eb429dd204a8ac5dbb5380ad
Carlos Solís <csolisr@azkware.net>
2026-06-11 16:06:43 +0000

    feat: Make the script more resilient to null values

--- friendica-find-largest-accounts.sh
+++ friendica-find-largest-accounts.sh
@@ -18,6 +18,13 @@ db="friendica"
         order by count(*) desc \
         limit 1000 \
     ); \
+    create temporary table tmp_authors_contact (\
+        select c.\`id\`, \
+        a.amount \
+        from contact as c \
+        right join tmp_authors as a \
+        on c.id <=> a.\`author-id\` \
+    ); \
     create temporary table tmp_owners (\
         select \`owner-id\`, \
         count(*) as amount \
@@ -25,6 +32,13 @@ db="friendica"
         group by \`owner-id\` \
         order by count(*) desc \
         limit 1000 \
+    ); \
+    create temporary table tmp_owners_contact (\
+        select c.\`id\`, \
+        o.amount \
+        from contact as c \
+        right join tmp_owners as o \
+        on c.id <=> o.\`owner-id\` \
     ); \
     create temporary table tmp_causers (\
         select \`causer-id\`, \
@@ -34,20 +48,39 @@ db="friendica"
         order by count(*) desc \
         limit 1000 \
     ); \
-    select \
-        c.\`id\`, \
+    create temporary table tmp_causers_contact (\
+        select c.\`id\`, \
+        t.amount \
+        from contact as c \
+        right join tmp_causers as t \
+        on c.id <=> t.\`causer-id\` \
+    ); \
+    create temporary table tmp_contacts (\
+        select c.\`id\`,
+        c.\`gsid\`,
         c.name, \
-        c.url, \
-	    g.platform, \
-        (a.amount + o.amount + t.amount) as final_amount \
-    from contact as c \
-	right join tmp_authors as a \
-        on c.id = a.\`author-id\` \
-	right join tmp_owners as o \
-	    on c.id = o.\`owner-id\` \
-	right join tmp_causers as t \
-	    on c.id = t.\`causer-id\` \
-	inner join gserver as g \
-	    on g.id = c.gsid \
-    order by final_amount asc \
-	limit 1000;"
+        c.url \
+        from contact as c \
+        where c.\`id\` in (\
+            select \`id\` from tmp_authors_contact \
+        ) or c.\`id\` in (\
+            select \`id\` from tmp_owners_contact \
+        ) or c.\`id\` in (\
+            select \`id\` from tmp_causers_contact \
+        )\
+    ); \
+    select c.\`id\`, \
+    c.name, \
+    c.url, \
+    ifnull(g.platform, \"\") as platform, \
+    (ifnull(a.amount, 0) + ifnull(o.amount, 0) + ifnull(t.amount, 0)) as final_amount \
+    from tmp_contacts as c \
+    left join tmp_authors_contact as a \
+    on c.\`id\` = a.\`id\` \
+    left join tmp_owners_contact as o \
+    on c.\`id\` = o.\`id\` \
+    left join tmp_causers_contact as t \
+    on c.\`id\` = t.\`id\` \
+    left join gserver as g \
+    on g.\`id\` = c.\`gsid\` \
+    order by final_amount asc;"
